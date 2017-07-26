065cba04a9c18f1a2bfe0baa588f538d1f3edaf0
George <zhishao@163.com>
2017-07-26 14:15:40 +0800

    W: 集成了vue-html5-editor。（https://github.com/PeakTai/vue-html5-editor）

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -2,10 +2,11 @@
     <layout>
         <div slot="main">
             <h1>高领秀青葱队</h1>
+            <editor content="" :height="500" :z-index="1000" :auto-height="true" :show-module-name="false"></editor>
         </div>
 
-        <iframe slot="preview" src="http://devwx.golfgreenshow.com/#/team/CEDE230F-3FFB-4F30-9966-B4E6F236EEDA" id="show" width="100%" height="99%" marginheight="0" marginwidth="0" frameborder="0" scrolling="auto"></iframe>
-    </layout>
+<!--         <iframe slot="preview" src="http://devwx.golfgreenshow.com/#/team/CEDE230F-3FFB-4F30-9966-B4E6F236EEDA" id="show" width="100%" height="99%" marginheight="0" marginwidth="0" frameborder="0" scrolling="auto"></iframe>
+ -->    </layout>
 </template>
 <script>
 
@@ -27,4 +28,5 @@ export default {
 </script>
 
 <style scoped>
+@import '../../font-awesome-4.7.0/css/font-awesome.min.css'
 </style>

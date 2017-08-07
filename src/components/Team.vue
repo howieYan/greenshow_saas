6193e1d8ea7ce1b38336bd784b97f11eda05db07
Yanhao <yanhao@golfgreenshow.com>
2017-08-07 20:51:32 +0800

    tema 页面 加绑定

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -3,14 +3,13 @@
         <div slot="main">
             <div style="margin:20px;">
                 <h1>球队首页</h1>
-
+                <h3 style="text-align:center;font-size:22px;padding:5px 0;">{{ team.active.name }}</h3>
                 <div class="header1" v-bind:style="{ backgroud: team.active && team.active.facade ? `url(${team.active.facade}) no-repeat center`:'', 'background-color': !team.active || !team.active.facade ? '#CCCCCC':''}">
                   <ul class="row">
-                      <li class="back" onclick="closeFrame()"></li>
                       <li class="textCenter textCenter0 col">
                           <img :src="team.active && team.active.logo ? team.active.logo : ''" alt="" with="200px" height="200px">
                           <p style="color:#fff;font-size:16px;cursor: pointer;" >
-                          口号：{{ team.active && team.active.purpose ? team.active.purpose : '' }}
+                          口号{{ team.active && team.active.purpose ? team.active.purpose : '' }}
                           <Button type="primary" @click="openInput">修改口号</Button>
                           </p>
                           <Form ref="formInline" inline  v-show="!open">
@@ -25,10 +24,97 @@
                               <div class="col">
                                   <div>{{ team.active && team.active.averageScore ? team.active.averageScore : '无' }}</div>
                                   <b>平均成绩</b>
+                                  <Button type="primary" @click="openInputs">修改平均成绩</Button>
+                                  <Form ref="formInline" inline  v-show="!opens">
+                                      <Form-item>
+                                          <Input v-model="data.averageScore" placeholder="请输入平均成绩"></Input>
+                                      </Form-item>
+                                      <Form-item>
+                                          <Button type="primary" @click="closeInputs">确定</Button>
+                                      </Form-item>
+                                  </Form>
                               </div>
+                             
                               <div class="col">
                                   <div>{{ team.active && team.active.memberCount ? team.active.memberCount : '无' }}</div>
                                   <b>球队人数</b>
+                                  <Button type="primary" @click="temaopenInputs">修改平均成绩</Button>
+                                  <Form ref="formInline" inline  v-show="!openss">
+                                      <Form-item>
+                                          <Input v-model="data.memberCount" placeholder="请输入平均成绩"></Input>
+                                      </Form-item>
+                                      <Form-item>
+                                          <Button type="primary" @click="temacloseInputs">确定</Button>
+                                      </Form-item>
+                                  </Form>
+                              </div>
+                          </div>
+                          <div class="row text_color">
+                              <div class="col">
+                                  <p >成立时间</p>
+                                  <b>{{ team.active && team.active.time ? team.active.time : '无' }}</b>
+                                  <Date-picker v-model="data.time"
+                                      :open="opena"
+                                      format="yyyy年M月d日"
+                                      confirm
+                                      placement="top-start"
+                                      @on-change="handleChange"
+                                      @on-clear="handleClear"
+                                      @on-ok="handleOk" style="z-index:10000000;">
+                                      <a href="javascript:void(0)" @click="handleClick">
+                                          <Icon type="ios-calendar-outline"></Icon>
+                                          <template v-if="value3 === ''" style="color:#000;">选择日期</template>
+                                          <template v-else>{{ value3 }}</template>
+                                      </a>
+                                  </Date-picker>
+                              </div>
+                              <div class="col">
+                                  <p>现任会长</p>
+                                  <b @click="clickName">{{ team.active && team.active.CDRname ? team.active.CDRname : '张林峰' }}</b>
+                                  <Form ref="formInline" inline v-show="!nameclick">
+                                      <Form-item>
+                                          <Input v-model="data.captain" placeholder="请输入名字" style="width:80px;"></Input>
+                                      </Form-item>
+                                      <Form-item>
+                                          <Button type="primary" @click="closename">确定</Button>
+                                      </Form-item>
+                                  </Form>
+                              </div>
+                              <div class="col">
+                                  <p>现任队长</p>
+                                  <b @click="captainclick">{{ team.active && team.active.captain ? team.active.captain : '无' }}</b>
+                                  <Form ref="formInline" inline v-show="!clickcaptain">
+                                      <Form-item>
+                                          <Input v-model="data.captain" placeholder="请输入名字" style="width:80px;"></Input>
+                                      </Form-item>
+                                      <Form-item>
+                                          <Button type="primary" @click="closecaptain">确定</Button>
+                                      </Form-item>
+                                  </Form>
+                              </div>
+                              <div class="col">
+                                  <p>现任秘书</p>  
+                                  <b @click="secretary">{{ team.active && team.active.secretary ? team.active.secretary : '无' }}</b>
+                                  <Form ref="formInline" inline v-show="!secretarys">
+                                      <Form-item>
+                                          <Input v-model="data.secretary" placeholder="请输入名字" style="width:80px;"></Input>
+                                      </Form-item>
+                                      <Form-item>
+                                          <Button type="primary" @click="closesecretary">确定</Button>
+                                      </Form-item>
+                                  </Form>
+                              </div>
+                              <div class="col">
+                                  <p>联系电话</p>
+                                  <b @click="phoneclick">{{ team.active && team.active.phone ? team.active.phone : '无' }}</b>
+                                  <Form ref="formInline" inline v-show="!clickphone">
+                                      <Form-item>
+                                          <Input v-model="data.phone" placeholder="请输入手机号码" style="width:120px;"></Input>
+                                      </Form-item>
+                                      <Form-item>
+                                          <Button type="primary" @click="closephone">确定</Button>
+                                      </Form-item>
+                                  </Form>
                               </div>
                           </div>
                       </li>
@@ -52,7 +138,7 @@
                     </ul>
                 </div>
                 <p style="text-align:center;line-height:30px;font-size:16px;">球队简介</p>
-                <editor content="" :height="400" :z-index="1000" :auto-height="true" :show-module-name="false"></editor>
+                <editor content="" :height="400" :auto-height="true" :show-module-name="false"></editor>
             </div>
         </div>
     </layout>
@@ -72,6 +158,15 @@ export default {
       account: account,
       url: axios.defaults.baseURL + '/api5/Upload/Team',
       open: true,
+      opens: true,
+      openss: true,
+      nameclick: true,
+      clickcaptain: true,
+      secretarys: true,
+      clickphone: true,
+      opena: false,
+      value3: '',
+      opentime: true,
       team: team,
       data: {
         purpose: null
@@ -93,12 +188,68 @@ export default {
     openInput () {
       this.open = false
     },
-
     closeInput () {
       this.open = true
       this.save({ purpose: this.data.purpose })
     },
-
+    temaopenInputs () {
+      this.openss = false
+    },
+    temacloseInputs () {
+      this.openss = true
+      this.save({ purpose: this.data.memberCount })
+    },
+    openInputs () {
+      this.opens = false
+    },
+    closeInputs () {
+      this.opens = true
+      this.save({ purpose: this.data.averageScore })
+    },
+    timeopen () {
+      this.opentime = false
+    },
+    timeclose () {
+      this.opentime = true
+      this.save({ purpose: this.data.averageScore })
+    },
+    clickName () {
+      this.nameclick = false
+    },
+    closename () {
+      this.nameclick = true
+      this.save({ purpose: this.data.CRDname })
+    },
+    captainclick () {
+      this.clickcaptain = false
+    },
+    closecaptain () {
+      this.clickcaptain = true
+      this.save({ purpose: this.data.captain })
+    },
+    secretary () {
+      this.secretarys = false
+    },
+    closesecretary () {
+      this.secretarys = true
+      this.save({ purpose: this.data.secretary })
+    },
+    phoneclick () {
+      this.clickphone = false
+    },
+    closephone () {
+      this.clickphone = true
+      this.save({ purpose: this.data.phone })
+    },
+    handleClick () {
+      this.opena = !this.opena
+    },
+    handleClear () {
+      this.opena = false
+    },
+    handleOk () {
+      this.opena = false
+    },
     async loadData () {
       try {
         let result = await api.getTeam(this.id, 'manager')
@@ -157,6 +308,15 @@ h1 {
     padding-left: 20px;
     line-height: 80px;
     border: 1px solid #eee;
+}
+.text_color>div{
+  padding:5px;
+}
+.text_color>div>p,.text_color>div>b{
+  color:#fff;
+}
+.text_color>div>p{
+  padding:5px;
 }
 @import '../../font-awesome-4.7.0/css/font-awesome.min.css'
 </style>
